9e10a18d27c207b08cebc87c48f747ed34993a1a
Sam <super8989@gmail.com>
2020-10-26 16:11:19 -0400

    Basic skeleton for footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faHeart,
+	faComment,
+	faPaperPlane,
+	faBookmark,
+} from '@fortawesome/free-regular-svg-icons';
+
 import './Footer.scss';
 
 function Footer() {
 	return (
 		<div className='Footer'>
-			<h1>Footer</h1>
+			<div className='Footer__left'>
+				<FontAwesomeIcon icon={faHeart} />
+				<FontAwesomeIcon icon={faComment} />
+				<FontAwesomeIcon icon={faPaperPlane} />
+			</div>
+			<div className='Footer__right'>
+				<FontAwesomeIcon icon={faBookmark} />
+			</div>
 		</div>
 	);
 }
