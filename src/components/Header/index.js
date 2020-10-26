3e3b957ecc779d1f85aceecb2a033f4e859567f7
Sam <super8989@gmail.com>
2020-10-26 15:18:48 -0400

    Basic skeleton for Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,16 @@ import './Header.scss';
 function Header() {
 	return (
 		<div className='Header'>
-			<h1>Header</h1>
+			<div className='Header__left'>
+				<img src='https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ' />
+				<div className='Header__left--username'>
+					<span>Username</span>
+					<span>sponsored</span>
+				</div>
+			</div>
+			<div className='Header__right'>
+				<span>...</span>
+			</div>
 		</div>
 	);
 }
