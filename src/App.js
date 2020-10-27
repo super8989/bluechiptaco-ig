4ae6c417957219efd4a4fadb5bcdcb7835cd4709
Sam <super8989@gmail.com>
2020-10-26 22:03:00 -0400

    Move state to Post component and map over data to display a list of Posts

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
 import Post from './components/Post';
 import data from './data/data.json';
 
-const post = data.feed[0];
+// const post = data.feed[0];
 
 function App() {
-	const [isLiked, setIsLiked] = useState(post.isLiked);
-	const [isBookmarked, setIsBookmarked] = useState(post.isBookmarked);
-
-	const likePost = () => {
-		setIsLiked(!isLiked);
-	};
-
-	const bookmarkPost = () => {
-		setIsBookmarked(!isBookmarked);
-	};
-
 	return (
 		<div className='App'>
-			<Post
-				post={post}
-				likePost={likePost}
-				isLiked={isLiked}
-				bookmarkPost={bookmarkPost}
-				isBookmarked={isBookmarked}
-			/>
+			{/* <Post post={post} /> */}
+			{data.feed.map((post, index) => {
+				return <Post key={index} post={post} />;
+			})}
 		</div>
 	);
 }
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../Footer';
 import Header from '../Header';
 import Media from '../Media';
 import './Post.scss';
 
-function Post({ post, likePost, isLiked, bookmarkPost, isBookmarked }) {
+function Post({ post }) {
+	const [isLiked, setIsLiked] = useState(post.isLiked);
+	const [isBookmarked, setIsBookmarked] = useState(post.isBookmarked);
+
+	const likePost = () => {
+		setIsLiked(!isLiked);
+	};
+
+	const bookmarkPost = () => {
+		setIsBookmarked(!isBookmarked);
+	};
+
 	return (
 		<div className='Post'>
 			<Header post={post} />
